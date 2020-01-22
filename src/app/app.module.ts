e6a785832d135713adfe1f4253cb8c5a64596bd6
Indraraj <indraraj@infiniteit.biz>
2020-01-22 16:57:41 +0530

    feat: camera/base64 add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
+import { Camera } from "@ionic-native/camera/ngx";
+import { WebView } from "@ionic-native/ionic-webview/ngx";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { Base64 } from "@ionic-native/base64/ngx";
+import { File } from "@ionic-native/file/ngx";
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +20,10 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    WebView,
+    Base64,
+    File,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,96 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Camera, CameraOptions } from "@ionic-native/camera/ngx";
+import { WebView } from "@ionic-native/ionic-webview/ngx";
+import { DomSanitizer } from "@angular/platform-browser";
+import { Base64 } from "@ionic-native/base64/ngx";
+import { File } from "@ionic-native/file/ngx";
 
+declare const window: any;
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  public imgUrl: any;
 
-  constructor() {}
+  constructor(
+    private camera: Camera,
+    private webView: WebView,
+    public _dom: DomSanitizer,
+    private _base64: Base64,
+    private _file: File
+  ) {}
 
+  takePicture(source: string) {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    if (source == "1") {
+      options.sourceType = this.camera.PictureSourceType.CAMERA;
+    } else if (source == "2") {
+      options.sourceType = this.camera.PictureSourceType.PHOTOLIBRARY;
+    }
+    this.camera.getPicture(options).then(
+      async imageData => {
+        console.log(this.webView.convertFileSrc(imageData), "convertFileSrc");
+        this.imgUrl = this.webView.convertFileSrc(imageData);
+        alert(this.webView.convertFileSrc(imageData));
+        console.log(imageData, "imageData");
+        alert(imageData);
+        let blob = await fetch(this.webView.convertFileSrc(imageData)).then(r =>
+          r.blob()
+        );
+        console.log(blob, "blob");
+        alert(blob);
+        this._base64.encodeFile(imageData).then(base64str => {
+          console.log(base64str);
+          alert(base64str);
+        });
+
+        window.resolveLocalFileSystemURL(imageData, fileEntry => {
+          fileEntry.file(file => {
+            console.log(file, "window file");
+            alert(file + "window file");
+            console.log(file.localURL, "localUrl");
+            this._base64
+              .encodeFile(file.localURL)
+              .then(base64LocalURL => {
+                console.log(base64LocalURL, "base64LocalUrl");
+              })
+              .catch(e => console.log(e, "base64localError"));
+          });
+        });
+
+        // if (imageData.indexOf("file://") == -1) {
+        //   imageData = "file://" + imageData;
+        // }
+        this._file.resolveLocalFilesystemUrl(imageData).then(entry => {
+          console.log(entry);
+          alert("entry" + entry);
+          // console.log(
+          //   entry.toInternalURL(),
+          //   entry.toURL(),
+          //   entry.fullPath,
+          //   entry.nativeURL
+          // );
+          // if (imageData.indexOf("file://") == -1) {
+          //   imageData = "file://" + imageData;
+          // }
+
+          // entry.file(rfile => {
+          //   console.log(rfile, "real file");
+          //   alert("real file" + rfile);
+          // });
+        });
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
